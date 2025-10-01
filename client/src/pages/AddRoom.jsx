182eadcb94fd81e20692591c501f26bb9b37ae2a
Jafar Hussain <jaffarhussssain@gmail.comgit add .git config --global user.email jaffarhussssain@gmail.com>
2025-10-02 02:11:41 +0500

    initial commit

--- client/src/pages/AddRoom.jsx
+++ client/src/pages/AddRoom.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useState } from "react";
+
+import Title from "../components/Title";
+import { assets } from "../assets/assets";
 
 export default function AddRoom() {
+  const [images, setImages] = useState({
+    1: null,
+    2: null,
+    3: null,
+    4: null,
+  });
+
+  const [inputs, setInputs] = useState({
+    roomType: "",
+    pricePerNight: "0",
+    amenities: {
+      "Free Wifi": false,
+      "Free Breakfast": false,
+      "Room Services": false,
+      "Mountain View": false,
+      "Pool Access": false,
+    },
+  });
   return (
-    <div>
-      
-    </div>
-  )
+    <form>
+      <Title
+        align="left"
+        font="outfit"
+        title="Add Room"
+        subTitle="Fill in the detail carfully anf 
+    accurate room detail, pricing and amenities, to enhnace the user booking experience "
+      />
+
+      {/* upload area for the images */}
+      <p className="text-gray-800 mt-10">Images</p>
+      <div className="grid grid-cols-2 sm:flex gap-4 my-2  flex-wrap">
+        {Object.keys(images).map((key) => (
+          <label htmlFor={`roomImages${key}`} key={key}>
+            <img
+              className="max-h-13 cursor-pointer opcity-80"
+              src={
+                images[key]
+                  ? URL.createObjectURL(images[key])
+                  : assets.uploadArea
+              }
+              alt=""
+            />
+            <input
+              type="file"
+              accept="image/*"
+              id={`roomImages${key}`}
+              hidden
+              onChange={(e) =>
+                setImages({ ...images, [key]: e.target.files[0] })
+              }
+            />
+          </label>
+        ))}
+      </div>
+    </form>
+  );
 }
--- client/src/pages/hotelOwner/Dashboard.jsx
+++ client/src/pages/hotelOwner/Dashboard.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Title from "../../components/Title";
+import { assets, dashboardDummyData } from "../../assets/assets";
 
 export default function Dashboard() {
+  const [dashboardData, setDashboardData] = useState(dashboardDummyData);
   return (
-    <div>
+    <div className="">
       <Title
         align="left"
         font="outfit"
@@ -12,19 +14,89 @@ export default function Dashboard() {
         subTitle="Monitor your room listings, track bokings and analyze revenue-all in one place.
       Stay update with real-time insights to ensure smooth operations."
       />
-
       <div className="flex gap-4 my-8">
-
         {/* total booking */}
 
-        <div>
+        <div className="bg-primary/3 border border-primary/10 flex p-4 pr-8 ">
+          <img
+            src={assets.totalBookingIcon}
+            alt=""
+            className="max-sm:hidden h-10"
+          />
 
+          <div className="flex flex-col sm:ml-4 font-medium">
+            <p className="text-blue-500 text-lg">Total Bookings</p>
+            <p className="text-neutral-400 text-base">
+              {dashboardData.totalBookings}
+            </p>
+          </div>
         </div>
 
         {/* total revenue */}
-        <div>
-            
+        <div className="bg-primary/3 border border-primary/10 flex p-4 pr-8">
+          <img
+            src={assets.totalRevenueIcon}
+            alt=""
+            className="max-sm:hidden h-10"
+          />
+
+          <div className="flex flex-col sm:ml-4 font-medium">
+            <p className="text-blue-500 text-lg">Total Revenue</p>
+            <p className="text-neutral-400 text-base">
+              $ {dashboardData.totalRevenue}
+            </p>
+          </div>
         </div>
+      </div>
+      {/* Recent booking */}
+      <h2 className="text-xl text-blue-950/70 font-medium mb-5">
+        Recent Booking
+      </h2>
+      <div className="w-full max-w-3xl text-left  border border-gray-300 overflow-y-scroll max-h-80 rounded-lg">
+        <table className="w-full">
+          <thead className="bg-gray-50">
+            <tr>
+              <th className="py-3 px-4 text-gray-800 font-medium">User Name</th>
+              <th className="py-3 px-4 text-gray-800 font-medium mx-sm:hidden">
+                Room Name
+              </th>
+              <th className="py-3 text-center px-4 text-gray-800 font-medium">
+                Total Amount
+              </th>
+              <th className="py-3 text-center px-4 text-gray-800 font-medium">
+                Payment Status
+              </th>
+            </tr>
+          </thead>
+
+          <tbody className="text-sm">
+            {dashboardData.bookings.map((item, index) => (
+              <tr key={index}>
+                <td className="py-3 px-4 text-gray-700 border-t border-gray-300">
+                  {item.user.username}
+                </td>
+                <td className="py-3 px-4 text-gray-700 border-t max-sm:hidden border-gray-300">
+                  {item.room.roomType}
+                </td>
+
+                <td className="py-3 px-4 text-gray-700 border-t border-gray-300 text-center">
+                  $ {item.totalPrice}
+                </td>
+                <td className="py-3 px-4 border-t border-gray-300 flex">
+                  <button
+                    className={`py-1 px-3 text-sm rounded-full mx-auto ${
+                      item.isPaid
+                        ? "bg-green-200 text-green-600"
+                        : "bg-amber-200 text-yellow-600"
+                    }`}
+                  >
+                    {item.isPaid ? "Completed" : "Pending"}
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
